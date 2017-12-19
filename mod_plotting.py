4707cb03cbddb8052349ca7a58f0f93635e019a7
borisz264 <borzin264@gmail.com>
2017-12-19 18:56:10 -0500

    changed some labels. added interactive scatterplot

--- mod_plotting.py
+++ mod_plotting.py
@@ -525,8 +525,8 @@ def mutation_rate_scatter(libraries, out_prefix, nucleotides_to_count='ATCG', ex
                 else:
                     x.append(nucleotide.mutation_rate)
                     y.append(nucleotide.get_control_nucleotide().mutation_rate)
-        plot.set_xlabel("%s mismatch_fraction" % (library.lib_settings.sample_name), fontsize = 8)
-        plot.set_ylabel("%s mismatch_fraction" % (library.get_normalizing_lib_with_mod().lib_settings.sample_name), fontsize = 8)
+        plot.set_xlabel("%s misincorporation fraction" % (library.lib_settings.sample_name), fontsize = 8)
+        plot.set_ylabel("%s misincorporation fraction" % (library.get_normalizing_lib_with_mod().lib_settings.sample_name), fontsize = 8)
         plot.set_yscale('log')
         plot.set_xscale('log')
         x=numpy.array(x)
@@ -543,6 +543,80 @@ def mutation_rate_scatter(libraries, out_prefix, nucleotides_to_count='ATCG', ex
         plot_figs.append(plot)
         plot_index+=1
     plt.savefig(output_file, transparent='True', format='pdf')
+
+def scatter_interactive(libraries, out_prefix, nucleotides_to_count='ATCG', exclude_constitutive=False,
+                         max_fold_reduction=0.001, max_fold_increase=100):
+    """
+
+    :param libraries:
+    :param out_prefix:
+    :param nucleotides_to_count:
+    :param exclude_constitutive:
+    :return: for each library use bokeh to plot an interactive plot of average magnitude of signal (experimental+control)/2
+            vs log10 fold change (experimental/control).
+            Protected and de-protected calls will be colored, based on a fold change cutoff and confidence interval.
+            All nucleotides will be labelled on mouseover.
+    """
+    from bokeh.plotting import figure, output_file, show, save, ColumnDataSource, gridplot
+    from bokeh.models import Range1d
+    from bokeh.models import HoverTool
+    from collections import OrderedDict
+
+    # output to static HTML file
+    output_file("%s.html" % (out_prefix))
+    plot_figs=[]
+
+    for library in libraries:
+        x, y, annotation = [], [], []
+        prot_x, prot_y, prot_annotation = [], [], []
+        deprot_x, deprot_y, deprot_annotation = [], [], []
+        for rRNA_name in library.rRNA_mutation_data:
+            for position in library.rRNA_mutation_data[rRNA_name].nucleotides:
+                nucleotide = library.rRNA_mutation_data[rRNA_name].nucleotides[position]
+                if (exclude_constitutive and nucleotide.exclude_constitutive)or nucleotide.identity not in nucleotides_to_count:
+                    continue
+                else:
+                    protection_call = nucleotide.determine_protection_status(confidence_interval=library.experiment_settings.get_property('confidence_interval_cutoff'),
+                                                                   fold_change_cutoff=library.experiment_settings.get_property('fold_change_cutoff'))
+                    control_fold_change = nucleotide.get_control_fold_change_in_mutation_rate()
+                    avg_mutation_rate = (nucleotide.mutation_rate+nucleotide.get_control_nucleotide().mutation_rate)/2.0
+                    if control_fold_change == 0:
+                        control_fold_change = max_fold_reduction
+                    elif control_fold_change == float('inf'):
+                        control_fold_change = max_fold_increase
+                    if protection_call == 'no_change':
+                        x.append(nucleotide.get_control_nucleotide().mutation_rate)
+                        y.append(nucleotide.mutation_rate)
+                        annotation.append('%s_%s%d' %(rRNA_name,nucleotide.identity,position))
+                    elif protection_call == 'deprotected':
+                        deprot_x.append(nucleotide.get_control_nucleotide().mutation_rate)
+                        deprot_y.append(nucleotide.mutation_rate)
+                        deprot_annotation.append('%s_%s%d' %(rRNA_name,nucleotide.identity,position))
+                    elif protection_call == 'protected':
+                        prot_x.append(nucleotide.get_control_nucleotide().mutation_rate)
+                        prot_y.append(nucleotide.mutation_rate)
+                        prot_annotation.append('%s_%s%d' %(rRNA_name,nucleotide.identity,position))
+        source = ColumnDataSource(data=dict(x = x, y = y, label = annotation))
+        prot_source = ColumnDataSource(data=dict(x = prot_x, y = prot_y, label = prot_annotation))
+        deprot_source = ColumnDataSource(data=dict(x = deprot_x, y = deprot_y,
+                                                   label = deprot_annotation))
+        TOOLS = "pan,wheel_zoom,reset,save,hover"
+        PlotFig = figure(x_axis_label = "%s misincorporation fraction" % (library.get_normalizing_lib_with_mod().lib_settings.sample_name),
+                         y_axis_label = "%s misincorporation fraction" % (library.lib_settings.sample_name),
+                         y_axis_type="log", x_axis_type="log", tools=TOOLS, toolbar_location="right")
+        PlotFig.circle("x", "y", size = 5, source=source, color=mod_utils.bokeh_black)
+        PlotFig.circle("x", "y", size = 5, source=prot_source, color=mod_utils.bokeh_vermillion)
+        PlotFig.circle("x", "y", size = 5, source=deprot_source, color=mod_utils.bokeh_bluishGreen)
+        PlotFig.x_range = Range1d(start=0.00001, end=1)
+        PlotFig.y_range = Range1d(start=0.00001, end=1)
+
+        #adjust what information you get when you hover over it
+        Hover = PlotFig.select(dict(type=HoverTool))
+        Hover.tooltips = OrderedDict([("nuc", "@label")])
+        plot_figs.append([PlotFig])
+    p = gridplot(plot_figs)
+    save(p)
+
 
 def ma_plots_by_count(libraries, out_prefix, nucleotides_to_count='ATCG', exclude_constitutive=False,
              max_fold_reduction=0.001, max_fold_increase=100, lowess_correct=False):
